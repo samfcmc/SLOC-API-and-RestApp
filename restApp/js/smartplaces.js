51b102657fcddaef6351e071ff00cdfeda9ffec7
Samuel Coelho <samuelfcmc@gmail.com>
2015-07-08 16:17:07 +0100

    Added functions to get beacons from the mobile app

--- restApp/js/smartplaces.js
+++ restApp/js/smartplaces.js
@@ -1,5 +1,12 @@
 window.SmartPlaces = {
+  tagFound: function() {
+    throw "You need to define a call back for when a tag is found. " +
+      "Call SmartPlaces.onTagFound(callback)";
+  },
   hello: function() {
     alert("hello, just testing");
+  },
+  onTagFound: function(callback) {
+    this.tagFound = callback;
   }
 };
