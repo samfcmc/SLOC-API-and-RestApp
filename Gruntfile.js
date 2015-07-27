991edecb0c6410395a513800656d63d13ddc59ee
Samuel Coelho <samuelfcmc@gmail.com>
2015-07-27 11:28:09 +0100

    Improved build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,9 @@
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-bower-task');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   require('load-grunt-tasks')(grunt);
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Project configuration.
   grunt.initConfig({
@@ -16,16 +18,35 @@ module.exports = function(grunt) {
     php: {
         dist: {
             options: {
-              keepalive: true,
+              keepalive: false,
               open: true,
               port: 9000
             }
+        },
+        watch: {
+
         }
+    },
+    copy: {
+      bower: {
+        src: 'bower_components/',
+        dest: 'restApp/bower_components',
+      },
+    },
+    watch: {
+      bower: {
+        files: ['bower.json'],
+        tasks: ['bower:install']
+      },
+      bowercomponents: {
+        files: ['bower_components/**/*'],
+        tasks: ['copy:bower']
+      }
     }
   });
 
   // Default task(s).
-  grunt.registerTask('default', ['bower', 'php']);
+  grunt.registerTask('default', ['bower', 'php:dist', 'php:watch', 'watch']);
   grunt.registerTask('dist', ['bower']);
 
 };
